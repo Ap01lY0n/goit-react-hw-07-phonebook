f3781ca66d20678c55f9eb6cf169eb009feb6bd7
Ap01lY0n <itstep22813@gmail.com>
2023-12-03 23:44:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,26 @@ import PhoneBookForm from './PhoneBookForm/PhoneBookForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import { Title } from './App.styled';
+import { selectContacts } from '../redux/selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { fetchContacts } from "../redux/fetchAPI";
 
 const App = () => {
+  const contacts = useSelector(selectContacts);
 
+  const dispatch = useDispatch();
+    useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch])
+  
   return (
     <div>
       <Title>Phonebook</Title>
       <PhoneBookForm />
       <Filter />
       <Title>Contacts</Title>
-      <ContactList />
+      {contacts.length !== 0 && <ContactList/>}
     </div>
   );
 };
